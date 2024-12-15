8bd428bdc6479226487755e0652ed2119f047d63
Seonghun Jeong <tjdgns5506@gmail.com>
2024-12-15 17:34:36 +0900

    chore: initialize Spring Boot project

---
+++ src/main/java/jsh/couponissueservicespringwebmvc/CouponIssueServiceSpringWebMvcApplication.java
@@ -0,0 +1,13 @@
+package jsh.couponissueservicespringwebmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponIssueServiceSpringWebMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponIssueServiceSpringWebMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jsh/couponissueservicespringwebmvc/CouponIssueServiceSpringWebMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package jsh.couponissueservicespringwebmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponIssueServiceSpringWebMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
